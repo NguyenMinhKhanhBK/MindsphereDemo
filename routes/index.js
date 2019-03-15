fdb328cc498500ce7e800e441ec74355354ca0e2
NguyenMinhKhanhBK <nmkhanhbk@gmail.com>
2019-03-15 22:56:58 +0700

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET map page. */
 router.get('/', function(req, res, next) {
+  res.render('mapPage', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
   res.render('homePage', { title: 'Express' });
 });
 
